d622fe6c7852ee9268cfe03e7b1e70b7b5cffd8e
Laurens Corijn <laurens.corijn.y7373@student.hogent.be>
2023-01-31 22:40:23 +0100

    .

--- src/app/language-switch/language-switch.component.ts
+++ src/app/language-switch/language-switch.component.ts
@@ -8,24 +8,11 @@ import { Router } from '@angular/router';
 })
 export class LanguageSwitchComponent {
   constructor(private router: Router) {
-    console.log(location.pathname)
-    console.log(location.origin)
-    //console.log(location.host)
-    //console.log(location.origin)
 
-    const parts = location.href.split("/")
-    const length = parts.length
-    //console.log(length)
-    //console.log(parts)
   }
 
   changeLanguage(value: string) {
-    //const pathName = location.pathname
-
-    //this.router.navigate([`${value}`])
-    //const test = `${location.origin}/${value}`
-    //console.log(test)
-    this.router.navigateByUrl(`/${value}`)
+    location.href=`${location.origin}/${value}`
   }
 }
 
